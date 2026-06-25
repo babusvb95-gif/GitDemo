96c48fe039943976987d728223aa5aa43b46cbb4
Babumanikandan <babusvb95@gmail.com>
2026-06-25 20:04:28 +0530

    Tester A changes done

--- src/Inheritance/ParentClassDemo.java
+++ src/Inheritance/ParentClassDemo.java
@@ -13,18 +13,7 @@ public void Brake() {
 		System.out.println("Brake Code Implemented");
 	}
 	
-public void Type() {
-	
-	System.out.println("Type Code Implemented");
-}
-public void TypeA() {
-	
-	System.out.println("Type Code Implemented");
-}
-public void TypeB() {
-	
-	System.out.println("Type Code Implemented");
-}
+
 
 	
 	
